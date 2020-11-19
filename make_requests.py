1258d26f5769f3d5f433b517e9cff0884d6f4ec0
Kristofer <kris@youngers.org>
2020-11-19 13:32:05 -0500

    need to do the filename correctly

--- make_requests.py
+++ make_requests.py
@@ -18,13 +18,16 @@ def test_x():
 # test_x()
 
 def saveJson(seq_num, jsondata):
+    if jsondata == None:
+        print("No Json Data")
+        return
     filename = "./dataNOAA_locations"+seq_num+".json"
     with open(filename, 'w') as json_file:
         json.dump(jsondata, json_file)
 
 
 def call_NOAA():
-    u = "https://www.ncdc.noaa.gov/cdo-web/api/v2/locations/?offset=XX&limit=XX"
+    u = "https://www.ncdc.noaa.gov/cdo-web/api/v2/locations/?offset=1&limit=25"
     limit = 1000
     offset = 1
     ctype = "application/json"
@@ -37,6 +40,7 @@ def call_NOAA():
     r = rq.get(u, headers=headers)
     #if r.status_code != "200":
     print(r.status_code, r.reason)
+    saveJson(offset, r.json())
     print(r.json())
 
 
